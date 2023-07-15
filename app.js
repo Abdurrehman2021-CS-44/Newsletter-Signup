5dd9f8517b3796890497ea145f808ed108f99a73
Abdurrehman_Khan <abdurrehmanbinfaheem@gmail.com>
2023-07-15 17:19:42 +0500

    Complete newsletter app with database

--- app.js
+++ app.js
@@ -1,27 +1,79 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require("request");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+const md5 = require("md5");
 
 const app = express();
 
-app.use(express.static("Public"));
-
 app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs")
+app.use(express.static("public"))
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/signup.html");
-    // <link href="styles.css" rel="stylesheet" />
+mongoose.connect("mongodb://127.0.0.1/newsletterDB")
+.then(()=>{
+    console.log("Database connected.");
+})
+.catch((err)=>{
+    console.log(err);
 });
 
-app.post("/", function(req, res){
-    console.log(req.body.firstName);
-    console.log(req.body.lastName);
-    console.log(req.body.email);
+const signupSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    }
 });
 
-app.listen(3000, function(){
-    console.log("Server is running on 3000 port");
+const Newsletter = new mongoose.model("Newsletter", signupSchema)
+
+let added = false;
+
+app.get("/", (req, res)=>{
+    res.render("signup", {isAdded: added});
 });
 
-// API Key
-// 862b8417b4199f8a58615035a33dd5d0-us21
+app.post("/", (req, res)=>{
+
+    Newsletter.findOne({email: req.body.email})
+    .then((found)=>{
+        if (found){
+            added = true;
+            console.log(found);
+            console.log(added);
+            res.redirect("/");
+        }
+        else {
+            added = false;
+            const signup = Newsletter({
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email
+            });
+            signup.save()
+            .then(()=>{
+                res.render("success");
+            })
+            .catch((err)=>{
+                res.render("failure");
+            })
+        }
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+});
+
+app.listen(3000, ()=>{
+    console.log("Server is running on port 3000.");
+})
+
+
